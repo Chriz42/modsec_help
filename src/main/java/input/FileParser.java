02a98958942e68474a1faea1802cdea5dabd9fa3
chris <work.christian.giebel@gmail.com>
2020-12-03 18:28:43 +0100

    init

---
+++ src/main/java/input/FileParser.java
@@ -0,0 +1,12 @@
+package input;
+
+import java.io.BufferedReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FileParser {
+
+	public Map<String, String> parse(BufferedReader reader) {
+		return new HashMap<String, String>();
+	}
+}
