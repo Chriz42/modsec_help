bea18d23783d7d5527757d02e8a413f8096a5b35
Christian.Giebel <Christian.Giebel@P1WN0542.intern.bs>
2021-05-19 10:43:43 +0200

    read input output filename from main args

--- src/main/java/system/Main.java
+++ src/main/java/system/Main.java
@@ -5,6 +5,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Set;
@@ -19,8 +20,19 @@ import model.UrlPart;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		// TODO READ filename from args
-		InputStream inputStream = Main.class.getResourceAsStream("modsec.log");
+		// TODO help from args
+		// Test this args logic
+		List<String> argsList = Arrays.asList(args);
+		String logFileName = "modsec.log";
+		for (int i = 0; i < args.length; i++) {
+			switch (args[i].toLowerCase()) {
+			case "logfilename":
+				logFileName = args[i + 1];
+				break;
+			}
+		}
+
+		InputStream inputStream = Main.class.getResourceAsStream(logFileName);
 		BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 		HashMap<String, Set<String>> dataMap = new FileParser().parse(reader);
 		List<UrlPart> urlList = new UrlPartsCreator().parseRAWData(dataMap);
